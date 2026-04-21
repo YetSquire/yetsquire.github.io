2a7d342d7c5a1112a1ae68373d5516e4cd8307a1
Andy <andyyao15@gmail.com>
2026-04-20 23:49:04 -0700

    Updated colors

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,7 +1,8 @@
 export const TEXT_COLORS = {
     // Home: "#19b701ff",
-    About: "#2f97feff",
+    // About: "#2f97feff",
     // Post: "#b3ff00ff",
-    Blog: "#19b701ff",
-    Project: "rgb(204, 90, 55)",
+    Project: "#2f97feff",
+    Thought: "#19b701ff",
+    Both: "rgb(41, 208, 199)"
 };
